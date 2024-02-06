29181207d46ca0cb8e26f3b39f0f3ddb1169eaab
Kreoniz <kreoniz@mail.ru>
2024-02-06 16:59:20 +0600

    Add all basic methods to the LinkedList class

--- linked-list.js
+++ linked-list.js
@@ -44,7 +44,6 @@ export default class LinkedList {
 
   at(index) {
     let tmp = this.head();
-    console.log(tmp);
     let pointer = 0;
 
     while (pointer < index) {
@@ -57,6 +56,52 @@ export default class LinkedList {
     }
 
     return tmp;
+  }
+
+  pop() {
+    let tmp = this.head();
+    while (tmp.nextNode.nextNode !== null) {
+      tmp = tmp.nextNode;
+    }
+
+    tmp.nextNode = null;
+  }
+
+  contains(value) {
+    let tmp = this.head();
+    while (tmp !== null) {
+      if (tmp.value === value) {
+        return true
+      };
+      tmp = tmp.nextNode;
+    }
+
+    return false;
+  }
+
+  find(value) {
+    let tmp = this.head();
+    let i = 0;
+    while (tmp !== null) {
+      if (tmp.value === value) {
+        return i;
+      };
+      tmp = tmp.nextNode;
+      i++;
+    }
+
+    return null;
+  }
+
+  toString() {
+    let tmp = this.head();
+    let string = '';
+    while (tmp !== null) {
+      string += `( ${tmp.value} ) -> `;
+      tmp = tmp.nextNode
+    }
+    string += 'null';
+    return string;
   }
 }
 
@@ -73,3 +118,13 @@ console.log();
 console.log(linkedList.head(), linkedList.tail());
 console.log();
 console.log(linkedList.at(5));
+console.log();
+console.log(linkedList.pop());
+console.log();
+console.log(linkedList.contains('Second'));
+console.log(linkedList.contains('Value'));
+console.log();
+console.log(linkedList.find('Third'));
+console.log(linkedList.at(3));
+console.log();
+console.log(linkedList.toString());
